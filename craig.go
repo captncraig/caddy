36596c223f9aae1013f6a0bf8c8c2d96f635ed8d
Craig Peterson <cpeterson@stackoverflow.com>
2016-02-05 23:11:59 -0700

    adding stats

--- craig.go
+++ craig.go
@@ -3,10 +3,12 @@ package main
 import (
 	"github.com/abiosoft/caddy-git"
 	"github.com/captncraig/caddy-realip"
+	"github.com/captncraig/caddy-stats"
 	"github.com/mholt/caddy/caddy"
 )
 
 func init() {
-	caddy.RegisterDirective("git", git.Setup, "internal")  //can go toward bottom
-	caddy.RegisterDirective("realip", realip.Setup, "tls") //goes almost at very top
+	caddy.RegisterDirective("git", git.Setup, "internal")     //can go toward bottom
+	caddy.RegisterDirective("realip", realip.Setup, "tls")    //goes almost at very top
+	caddy.RegisterDirective("stats", stats.Setup, "shutdown") //as high as I dare
 }

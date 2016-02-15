7cc0b0ef3ab49e0ff872ccaf4f5da9c57e2cb470
Craig Peterson <cpeterson@stackoverflow.com>
2016-02-15 01:33:33 -0700

    better functioning app wrapper

--- craig.go
+++ craig.go
@@ -10,7 +10,6 @@ import (
 	"github.com/mholt/caddy/caddy"
 	"github.com/mholt/caddy/caddy/setup"
 	"github.com/mholt/caddy/middleware"
-	"github.com/mitchellh/mapstructure"
 
 	"github.com/captncraig/hugomail/web"
 )
@@ -30,12 +29,8 @@ func init() {
 // convert a new struct factory and servemux creator into an official middleware
 func appToDirective(newConf func() interface{}, getMux func(interface{}) *http.ServeMux) caddy.SetupFunc {
 	return func(c *setup.Controller) (middleware.Middleware, error) {
-		m, err := readMap(c)
-		if err != nil {
-			return nil, err
-		}
 		conf := newConf()
-		err = mapstructure.WeakDecode(m, conf)
+		err := c.Unmarshal(conf)
 		if err != nil {
 			return nil, err
 		}
@@ -45,38 +40,6 @@ func appToDirective(newConf func() interface{}, getMux func(interface{}) *http.S
 			return myMux{mux}
 		}, nil
 	}
-}
-
-//read single key/value pairs from block into a map
-//expects:
-// - 1 and only one directive with this name
-// - No top level arguments
-// - No repeat config lines
-// really for mapping simple key/value into a struct
-func readMap(c *setup.Controller) (map[string]interface{}, error) {
-
-	r := map[string]interface{}{}
-	if !c.Next() {
-		return nil, c.Err("Expect directive")
-	}
-	if args := c.RemainingArgs(); len(args) != 0 {
-		return nil, c.ArgErr()
-	}
-	for c.NextBlock() {
-		k := c.Val()
-		if r[k] != nil {
-			return nil, c.Errf("Repeat key %s", k)
-		}
-		args := c.RemainingArgs()
-		if len(args) != 1 {
-			return nil, c.ArgErr()
-		}
-		r[k] = args[0]
-	}
-	if c.Next() {
-		return nil, c.Err("Expect only one directive")
-	}
-	return r, nil
 }
 
 type myMux struct {
